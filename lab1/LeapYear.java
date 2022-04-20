6e032ae9d64a70dec8f7456e6f7d5f44e71535d8
jy_hpz66 <jiangyunacw@gmail.com>
2022-04-20 19:39:52 +0800

    add isLeapYear function.

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,6 +6,9 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+	public static boolean isLeapYear(int year){
+    return (year % 400 == 0 | (year % 4 == 0 & year % 100 != 0));
+    }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
