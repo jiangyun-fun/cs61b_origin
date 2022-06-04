0b7b361a0bd889c5a7c45455f115c16fe5fcdede
JiangYun2001 <jiangyunacw@gmail.com>
2022-06-04 19:51:27 +0800

    finish task 2.

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,12 @@
 public class Palindrome {
+    public Deque<Character> wordToDeque(String word){
+        LinkedListDeque<Character> lld = new LinkedListDeque<>();
+
+        int i = 0;
+        while (i != word.length()) {
+            lld.addLast(word.charAt(i));
+            i++;
+        }
+        return lld;
+    }
 }
